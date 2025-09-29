20123bfe22912f54c1af1cbaf5b88b277b792c5c
ka4543 <kamelhanna299@gmail.com>
2025-09-29 05:40:26 +0200

    Hü Testklasse

---
+++ POS/2025-09_Hanna_Bmi_Wert_Testklasse/Student.java
@@ -0,0 +1,82 @@
+public class Student {
+
+    private String vorname;   // Name Student 
+    private float kg;      // Gewicht in Kilogramm kommaZahl
+    private int cm;        // Größe in Zentimeter GanzZahl
+    private char gender;   // Geschlecht
+
+    public Student(String vorname, float kg, int cm, char gender) {
+        this.setName(vorname);       // Namen  speichern
+        this.setKg(kg);           // Gewicht  speichern
+        this.setCm(cm);           // Größe  speichern
+        this.setGender(gender);   // Geschlecht  speichern
+    }
+
+    public void setName (String vorname){
+        if( vorname.length()< 3 || vorname.length() >50 ){
+            throw new IllegalArgumentException("Name muss zwi. 3-50 Buchstaben sein! Grossbuchstaben möglich.");
+        }
+        this.vorname=vorname;
+    }
+  
+    public void setKg(float kg){
+        if(kg < 2f || kg > 550f){
+            throw new IllegalArgumentException("Masse muss zwi. 2-550 sein");
+        }
+        this.kg=kg;
+    }
+    
+    public void setCm(int cm){
+        if(cm <50 || cm > 230){
+            throw new IllegalArgumentException("Groesse muss zwi. 50- 230 sein");
+        }
+        this.cm=cm;
+        System.out.println ("Deine Groessse beträgt " + this.cm +" cm") ;
+
+    }
+    public void setGender(char gender) {
+        
+        this.gender = gender;
+    }
+
+    public float bmi() {
+        float meter = this.cm / 100.0f;
+        return this.kg / (meter * meter);
+    }
+
+    public String mannOderFrau() {
+        // wir vergleichen den Buchstaben
+        if (this.gender == 'm' || this.gender == 'M') return "männlich";  
+        if (this.gender == 'f' || this.gender == 'F') return "weiblich";  
+
+        return "Bitte gültige Daten eingeben!";
+    }
+
+    public String BmiKlasse() {
+        float bmi = this.bmi();  // BMI berechnen
+
+        // Männer 
+        if (this.gender == 'm' || this.gender == 'M') {
+            if (bmi < 20f) return "Untergewicht";      // 
+            else if (bmi <= 25f) return "Normalgewicht"; // 
+            else return "Übergewicht";                  // 
+        }
+        // Frauen
+        else if (this.gender == 'f' || this.gender == 'F') {
+            if (bmi < 19f) return "Untergewicht";      
+            else if (bmi <= 24f) return "Normalgewicht"; 
+            else return "Übergewicht";                  
+        }
+        else {
+            return "Fehler: kein gültiges Geschlecht";
+        }
+    }
+
+    public String printStudent() {
+        return "Name: " + this.vorname
+            + " (" + this.mannOderFrau() + "), "
+            + this.kg + "kg, "
+            + this.cm + "cm ("
+            + this.BmiKlasse() + ")";
+    }
+}
---
+++ POS/2025-09_Hanna_Bmi_Wert_Testklasse/StudentTest.java
@@ -0,0 +1,81 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class StudentTest
+    {
+        private Student Anna;
+        private Student Lara;
+        private Student Nicole;
+        
+        private Student Kamel;
+        private Student Alex;
+        private Student Marko;
+
+    
+    @BeforeEach
+    public void setUp()
+    {
+        System.out.println("SetUp");
+        Anna= new Student("Anna",50,180,'F');
+        Lara= new Student("Lara",70,150,'F');
+        Nicole= new Student("Nicole",50,150,'F');
+        
+        Kamel= new Student("Kamel",90,170,'m');
+        Alex= new Student("Alex",70,190,'m');
+        Marko= new Student("Marko",75,180,'m');
+    }
+    
+    @Test
+    public void TestAnna(){
+    assertEquals("Untergewicht", Anna.BmiKlasse(),"BMI von Anna");
+}
+@Test
+    public void TestLara(){
+        assertEquals("Übergewicht", Lara.BmiKlasse());
+}
+@Test
+    public void TestNicole(){
+        assertEquals("Normalgewicht", Nicole.BmiKlasse());
+}
+@Test
+    public void TestKamel(){
+        assertEquals("Übergewicht", Kamel.BmiKlasse());
+}
+@Test
+    public void TestAlex(){
+        assertEquals("Untergewicht", Alex.BmiKlasse());
+}
+@Test
+    public void TestMarko(){
+        assertEquals("Normalgewicht", Marko.BmiKlasse());
+}
+@Test
+    public void TestAnnaFloat(){
+        assertEquals(15.43, Anna.bmi(),0.1,"BMI von ANNA");
+}
+@Test
+    public void TestLaraFloat(){
+        assertEquals(31.11, Lara.bmi(),0.1,"BMI von LARA");
+    }
+        @Test
+    public void TestNicoleFloat(){
+        assertEquals(22.22, Nicole.bmi(),0.1,"BMI von Nicole");
+}
+@Test
+     public void TestKonstruktor(){
+      assertThrows(IllegalArgumentException.class, () -> {
+       new Student("Kamel", 80, 49,'m');
+        }, "Grösse muss zwi. 50-250 cm sein");
+    }
+    
+    
+    @AfterEach
+    public void tearDown()
+    {
+        System.out.println("SetUp");
+    }
+}
